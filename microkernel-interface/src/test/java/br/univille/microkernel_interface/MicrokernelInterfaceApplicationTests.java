46df0486bf640604c3be2b1b0396126deaabbae7
matheushagedorn <matheus.hagedorn@gmail.com>
2024-10-03 20:16:02 -0300

    alteração pom.xml e criação do interface

---
+++ microkernel-interface/src/test/java/br/univille/microkernel_interface/MicrokernelInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microkernel_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MicrokernelInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
